1eb93840ec4b0feab89d52470280a5c2eb8bfae5
Arthur Mondon <arthur@mondon.pro>
2024-06-21 22:43:01 +0200

    Innitial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# extract-etsy-orders-shipping-address
+
+> A chrome extension to extract all orders and their shipping address from Etsy.
+
+**Developer :** Arthur Mondon
+
+**June 2024**
+
+## Description
+
+This extension allows you to retrieve all orders and their shipping address from Etsy. 
+You can preview the total number of orders and the number of orders for your country.
+You can easily copy the orders addresses to your clipboard.
+
+## Installation
+
+- Télécharger le code source.
+- Décompresser l'archive.
+- Ouvrir Chrome ou Edge.
+- Se rendre sur la page Extensions ("[chrome://extensions/](chrome://extensions/)" ou "[edge://extensions/](edge://extensions/)");
+- Activer le mode développeur.
+- Cliquer sur "Charger l’extension décompressée".
+- Sélectionner le dossier racine de l'extension téléchargée décompressée. 
+- Enjoy !
+
+## Avertissements
+
+- Votre navigateur peut vous indiquer que cette extension accède aux données du site Web. Ceci est généré automatiquement, car le plugin analyse le code source HTML pour masquer intelligemment les bannières de notification. Il ne surveille ni n'enregistre votre historique de navigation ou vos informations personnelles. 
+- L'historique des actions est stocké localement et n'est jamais transmis en dehors de votre appareil.
---
+++ popup/popup.js
@@ -0,0 +1,181 @@
+function formatDestination(dest) {
+    const fields = ["name", "first-line", "second-line", "third-line"];
+    const seenValues = new Set();
+    const values = [];
+
+    // Function to capitalize the first letter of a string
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    // Add standard fields
+    fields.forEach(field => {
+        if (dest[field]) {
+            const lowerValue = dest[field].toLowerCase();
+            if (!seenValues.has(lowerValue)) {
+                seenValues.add(lowerValue);
+                values.push(capitalizeFirstLetter(dest[field]));
+            }
+        }
+    });
+
+    // Combine zip and city if they are not the same
+    const zip = dest["zip"] ? dest["zip"].trim() : '';
+    const city = dest["city"] ? capitalizeFirstLetter(dest["city"].trim()) : '';
+    const zipCity = zip === city ? zip : `${zip} ${city}`.trim();
+    if (zipCity) {
+        const lowerZipCity = zipCity.toLowerCase();
+        if (!seenValues.has(lowerZipCity)) {
+            seenValues.add(lowerZipCity);
+            values.push(zipCity);
+        }
+    }
+
+    // Add state if it's not the same as city
+    if (dest["state"] && dest["state"].toLowerCase() !== (dest["city"] ? dest["city"].toLowerCase() : '')) {
+        const lowerState = dest["state"].toLowerCase();
+        if (!seenValues.has(lowerState)) {
+            seenValues.add(lowerState);
+            values.push(capitalizeFirstLetter(dest["state"]));
+        }
+    }
+
+    // Add country-name last
+    if (dest["country-name"]) {
+        const lowerCountryName = dest["country-name"].toLowerCase();
+        if (!seenValues.has(lowerCountryName)) {
+            seenValues.add(lowerCountryName);
+            values.push(capitalizeFirstLetter(dest["country-name"]));
+        }
+    }
+
+    return values.join('<br>');
+}
+
+
+
+function copyToClipboard(text, element) {
+    navigator.clipboard.writeText(text).then(() => {
+        element.classList.add('copied');
+        setTimeout(() => {
+            element.classList.remove('copied');
+        }, 500);
+    }).catch(err => {
+        console.error('Failed to copy: ', err);
+    });
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const countButton = document.getElementById('countButton');
+    const list = document.createElement('ul');
+    const details = document.getElementById('details');
+
+    countButton.addEventListener('click', function () {
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            chrome.scripting.executeScript(
+                {
+                    target: { tabId: tabs[0].id },
+                    function: getDestinations
+                },
+                (results) => {
+                    console.log(results);
+
+                    if (results && results[0] && results[0].result) {
+                        const destinations = results[0].result;
+                        details.innerHTML = '';
+                        list.innerHTML = '';
+
+                        let franceDestinations = [];
+                        let otherDestinations = [];
+
+                        if (true) { // getCompleteAddress
+                            franceDestinations = destinations.filter(dest => dest["country-name"].includes('France'));
+                            otherDestinations = destinations.filter(dest => !dest["country-name"].includes('France'));
+                            destinations.forEach(dest => {
+                                const li = document.createElement('li');
+                                li.innerHTML = formatDestination(dest);
+                                li.addEventListener('click', () => {
+                                    copyToClipboard(li.innerText, li);
+                                });
+                                list.appendChild(li);
+                            });
+                        } else { // get only country
+                            console.log(destinations)
+                            franceDestinations = destinations.filter(dest => dest.includes('France'));
+                            otherDestinations = destinations.filter(dest => !dest.includes('France'));
+                            destinations.forEach(dest => {
+                                const li = document.createElement('li');
+                                li.textContent = dest;
+                                li.addEventListener('click', () => {
+                                    copyToClipboard(li.innerText, li);
+                                });
+                                list.appendChild(li);
+                            });
+                        }
+
+                        const franceCount = franceDestinations.length;
+                        const otherCount = otherDestinations.length;
+
+                        details.innerHTML = `
+
+                                <div class="counter">
+                                    <h3>Total number of orders</h3>
+                                    <h1>${destinations.length}</h1>
+                                </div>
+                            <div class="fr g0-5 w100">
+                                <div class="counter">
+                                    <img src="elements/france.svg" alt="Map">
+                                    <h3>France</h3>
+                                    <h1>${franceCount}</h1>
+                                </div>
+                                <div class="counter">
+                                    <img src="elements/map.svg" alt="Map">
+                                    <h3>Autre</h3>
+                                    <h1>${otherCount}</h1>
+                                </div>
+                            </div>
+                        `;
+
+                        if (list.children.length > 0){
+                            const listTitle = document.createElement('h2');
+                            listTitle.textContent = 'Destinations';
+                            listTitle.style.margin = '0.5rem auto 0 0 ';
+                            details.appendChild(listTitle);
+                        }
+                        details.appendChild(list);
+
+                    } else {
+                        result.textContent = 'No destinations found';
+                    }
+                }
+            );
+        });
+    });
+});
+
+
+function getDestinations() {
+    let destinations = [];
+    if (true) { // getCompleteAdress
+        const destinationButtons = document.querySelectorAll('section[aria-label="orders"] .panel-body .flag .flag-body .col-group .col-md-4 .wt-mt-xs-2 div button[aria-expanded="false"]');
+        destinationButtons.forEach(button => button.click());
+        const destinationElements = document.querySelectorAll('section[aria-label="orders"] .panel-body .flag .flag-body .col-group .col-md-4 .wt-mt-xs-2 div .address.break-word p');
+        destinations = Array.from(destinationElements).map(el => {
+            const spans = el.querySelectorAll('span');
+            const destination = {};
+            spans.forEach(span => {
+                destination[span.className] = span.textContent.trim();
+            });
+            return destination;
+        });
+    } else { // get only country
+        const destinationButtons = document.querySelectorAll('section[aria-label="orders"] .panel-body .flag .flag-body .col-group .col-md-4 .wt-mt-xs-2 div button[aria-expanded="true"]');
+        destinationButtons.forEach(button => button.click());
+        const destinationElements = document.querySelectorAll('section[aria-label="orders"] .panel-body .flag .flag-body .col-group .col-md-4 .wt-mt-xs-2 div .break-word .text-body-smaller:not(.strong) span span span:nth-child(2)');
+        console.log(destinationElements)
+        destinations = Array.from(destinationElements).map(el => el.textContent.trim());
+    }
+    return destinations;
+
+}
